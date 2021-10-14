d3eb584b87bed34e2443c18609c299ace0631cb3
Vinicius Gonçalves <70669306+Vyncs@users.noreply.github.com>
2021-10-14 16:23:50 -0300

    Add files via upload

---
+++ GerarParcelas/GerarParcelas/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace GerarParcelas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.cbbParcelas = new System.Windows.Forms.ComboBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.dgvParcelas = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.parcelas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvParcelas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbbParcelas
+            // 
+            this.cbbParcelas.FormattingEnabled = true;
+            this.cbbParcelas.Items.AddRange(new object[] {
+            "1x",
+            "2x",
+            "3x",
+            "4x",
+            "5x",
+            "6x",
+            "7x",
+            "8x",
+            "9x",
+            "10x",
+            "11x",
+            "12x"});
+            this.cbbParcelas.Location = new System.Drawing.Point(21, 48);
+            this.cbbParcelas.Name = "cbbParcelas";
+            this.cbbParcelas.Size = new System.Drawing.Size(69, 21);
+            this.cbbParcelas.TabIndex = 0;
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(96, 48);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 1;
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Location = new System.Drawing.Point(202, 48);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGerar.TabIndex = 2;
+            this.btnGerar.Text = "Gerar";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgvParcelas
+            // 
+            this.dgvParcelas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvParcelas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.parcelas,
+            this.valor});
+            this.dgvParcelas.Location = new System.Drawing.Point(21, 74);
+            this.dgvParcelas.Name = "dgvParcelas";
+            this.dgvParcelas.Size = new System.Drawing.Size(256, 338);
+            this.dgvParcelas.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Parcelas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Valor";
+            // 
+            // parcelas
+            // 
+            this.parcelas.DataPropertyName = "numeroParcela";
+            this.parcelas.HeaderText = "Parcelas";
+            this.parcelas.Name = "parcelas";
+            // 
+            // valor
+            // 
+            this.valor.DataPropertyName = "Valor";
+            dataGridViewCellStyle1.Format = "C2";
+            dataGridViewCellStyle1.NullValue = null;
+            this.valor.DefaultCellStyle = dataGridViewCellStyle1;
+            this.valor.HeaderText = "Valor";
+            this.valor.Name = "valor";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 424);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvParcelas);
+            this.Controls.Add(this.btnGerar);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.cbbParcelas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvParcelas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbbParcelas;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.DataGridView dgvParcelas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn parcelas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn valor;
+    }
+}
+
---
+++ GerarParcelas/GerarParcelas/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GerarParcelas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Parcela
+        {
+            public int numeroParcela { get; set; }
+            public double Valor { get; set; }
+        }
+
+        private void CalculaParcelas(int qtdeParcela)
+        {
+            double valorTotal = Convert.ToDouble(txtValor.Text);
+            int totalParcelas = qtdeParcela;
+            int contador = 0;
+            List<Parcela> lista = new List<Parcela>();
+            for (int i = 0; i < totalParcelas; i++)
+            {
+                contador++;
+                var parcela = new Parcela();
+                double Valora = Math.Round(valorTotal / qtdeParcela);
+                double valorDiferenca = valorTotal - Valora * qtdeParcela;
+                string valorb = !(i + 1 == qtdeParcela) ? Valora.ToString() : (Valora + valorDiferenca).ToString();
+
+                parcela.Valor = Convert.ToDouble(valorb);
+                parcela.numeroParcela = contador;
+
+                lista.Add(parcela);
+            }
+            dgvParcelas.AutoGenerateColumns = false;
+            dgvParcelas.DataSource = null;
+            dgvParcelas.DataSource = lista;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string escolha = cbbParcelas.SelectedItem.ToString();
+            switch (escolha)
+            {
+
+                case "1x":
+                    CalculaParcelas(1);
+                    break;
+                case "2x":
+                    CalculaParcelas(2);
+                    break;
+                case "3x":
+                    CalculaParcelas(3);
+                    break;
+                case "4x":
+                    CalculaParcelas(4);
+                    break;
+                case "5x":
+                    CalculaParcelas(5);
+                    break;
+                case "6x":
+                    CalculaParcelas(6);
+                    break;
+                case "7x":
+                    CalculaParcelas(7);
+                    break;
+                case "8x":
+                    CalculaParcelas(8);
+                    break;
+                case "9x":
+                    CalculaParcelas(9);
+                    break;
+                case "10x":
+                    CalculaParcelas(10);
+                    break;
+                case "11x":
+                    CalculaParcelas(11);
+                    break;
+                case "12x":
+                    CalculaParcelas(12);
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
